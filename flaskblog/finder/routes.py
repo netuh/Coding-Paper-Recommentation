d0eefc1f10401f31477c41b34f39c9daba56a6ad
Victor-Silva96 <victorsilva07101996@gmail.com>
2020-09-19 23:40:16 -0300

    Criando a tela de pesquisa por artigos ou autores, e sua respectiva rota, e também a tela de detalhes de um artigo com alguns conteudos como author titulo ano e local.

--- flaskblog/finder/routes.py
+++ flaskblog/finder/routes.py
@@ -1,8 +1,10 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, make_response, jsonify
 from flaskblog.models import Publication, Sampling
 from flaskblog.finder.forms import SelectArticleForm
 from flaskblog import db
 import scholarly
+import json
+from sqlalchemy import or_
 
 finder = Blueprint('finder', __name__)
 
@@ -64,12 +66,66 @@ def index():
     return render_template('finder/select.html', form=form)
 
 
+@finder.route("/finder/select_teste")
+def index_teste():
+    papers = Publication.query.all();
+    authors = get_authors_papers(papers)
+    titles = get_papers_title(papers)
+    dict_authors_titles = dict.fromkeys(titles + authors)
+    dict_authors_titles = json.dumps(dict_authors_titles)
+
+    return render_template('finder/select_teste.html', dict_authors_titles=dict_authors_titles, )
+
+
+def get_authors_papers(papers, authors=[]):
+    for paper in papers:
+        if (';' in paper.authors):
+            authors.extend(paper.authors.split(';'))
+        else:
+            authors.append(paper.authors)
+    return authors
+
+
+def get_papers_title(papers, titles=[]):
+    for paper in papers:
+        titles.append(paper.title)
+    return titles
+
+
 @finder.route("/search")
 def search():
     page = request.args.get('page', 1, type=int)
     pubs = Publication.query.order_by(
         Publication.year.desc()).paginate(page=page, per_page=5)
     return render_template('research.html', pubs=pubs)
+
+
+@finder.route("/search_teste", methods=['GET'])
+def search_teste():
+    search = request.args.get('search', "", type=str)
+    page = request.args.get('page', 1, type=int)
+    search = "%{}%".format(search)
+    publications = Publication.query.filter(
+        or_(Publication.title.like(search), Publication.authors.like(search))).paginate(per_page=7, page=page);
+    publications = deserialize_papers(publications)
+    publications["search"]=search;
+    res = make_response(json.dumps(publications), 200)
+    return res;
+
+
+def deserialize_papers(publications):
+    if (publications):
+        paper_author = []
+        for publication in publications.items:
+            paper_author.append({"id": publication.pub_id, "name": publication.title, "authors": publication.authors})
+
+        pages_conf = {"has_next": publications.has_next, "has_prev": publications.has_prev,
+                      "next_num": publications.next_num, "page": publications.page,
+                      "pages": [page for page in publications.iter_pages(left_edge=1, right_edge=1, left_current=3, right_current=3)],
+                      "prev_num": publications.prev_num}
+
+        paper_pages = {"papers": paper_author, "page_conf": pages_conf}
+        return paper_pages;
 
 
 @finder.route("/details/<int:pub_id>")
@@ -84,5 +140,20 @@ def details(pub_id):
     year = pub.year
     venue = pub.venue
     return render_template('detail.html', title=title, author=author,
+                           abstract=abstract, year=year, venue=venue,
+                           link=link)
+
+@finder.route("/details_teste/<int:pub_id>")
+def details_teste(pub_id):
+    pub = Publication.query.filter_by(pub_id=pub_id).first_or_404()
+    search_query = scholarly.search_pubs_query(pub.title)
+    paperData = next(search_query)
+    title = paperData.bib['title']
+    author = paperData.bib['author']
+    abstract = paperData.bib['abstract']
+    link = paperData.bib['url']
+    year = pub.year
+    venue = pub.venue
+    return render_template('detail_teste.html', title=title, author=author,
                            abstract=abstract, year=year, venue=venue,
                            link=link)
