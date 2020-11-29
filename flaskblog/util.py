e0fc1e09f7d42765833e1041189a4c3575e92177
Victor-Silva96 <#Ril071097>
2020-11-29 12:10:28 -0300

    Aumentando as tentativas para 5, caso o retorno seja 500.

--- flaskblog/util.py
+++ flaskblog/util.py
@@ -97,7 +97,7 @@ def select_authors(element_authors):
 def get_papers_google(search):
     client = ScraperAPIClient(API_KEY)
     url = 'https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q={0}&btnG='.format(search)
-    response = client.get(url=url).text
+    response = client.get(url=url, retry=5).text
     soup = BeautifulSoup(response,
                          'lxml')
 
